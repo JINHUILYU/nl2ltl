674e3c5afe2527849c22ce420be1f3bdda5a4ac5
JH Lyu <JH_Lyu@outlook.com>
2024-06-25 21:17:51 +0800

    添加prompt文件

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Natural language to Linear-time Temporal Logic
 Vue + Python + Flask + NodeJS
+# How to start
+1. Install the dependencies
+2. input the key of the LLM
+3. Run the frontend
+```
+npm run serve
+```
+4. Run the backend
+```
+python3 app.py
+```
+# Todo
+- [ ] 数据缓存
+- [ ] 数据导出
+- [ ] prompt文件选择
+- [ ] 模型选择
+- [ ] 历史信息显示
+- [x] 上下文记忆能力
+- [ ] 是否需要上下文信息
+  - [ ] 如果需要，则显示当前数据库保存的csv表格并选择所需要的文件或手动上传csv文件
+- [ ] 输入对应模型的key
--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import openai
+# import openai
 from openai import OpenAI
 import os
 
@@ -16,11 +16,18 @@ client = OpenAI(
     base_url="https://api.moonshot.cn/v1",
 )
 
-history = [
-    {"role": "system", "content": "你是 Kimi，由 Moonshot AI 提供的人工智能助手，你更擅长中文和英文的对话。你会为用户提供安全，有帮助，"
-                                  "准确的回答。同时，你会拒绝一切涉及恐怖主义，种族歧视，黄色暴力等问题的回答。Moonshot AI 为专有名词，"
-                                  "不可翻译成其他语言。"}
-]
+history = []
+
+
+def init():
+    # 读取prompt
+    promptFile = os.path.join(os.path.pardir, "prompts\\kimi.txt")
+    try:
+        with open(promptFile, encoding='utf-8') as file:
+            prompt = file.readline().strip("\n")  # 从prompt file中读取prompt, 此处为kimi
+    except UnicodeDecodeError as e:
+        print(f"Error reading file {promptFile}: {e}")
+    history.append({"role": "system", "content": prompt})
 
 
 @app.route('/process', methods=['POST'])
@@ -40,9 +47,9 @@ def process_data():
     history.append({"role": "assistant", "content": processed_data})
     # 获取处理后的数据
     response_data = {'processedData': processed_data}
-    print(response_data)
     return jsonify(response_data)
 
 
 if __name__ == '__main__':
+    init()
     app.run(debug=True)
